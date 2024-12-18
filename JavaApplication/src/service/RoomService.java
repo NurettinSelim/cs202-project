9e7cf9d468a7c827b8802c8cccbbfac0dd905bc5
NurettinSelim <nurettinselim03@gmail.com>
2024-12-18 11:01:27 +0300

    RoomService

--- JavaApplication/src/service/RoomService.java
+++ JavaApplication/src/service/RoomService.java
@@ -1,22 +1,17 @@
 package service;
 
 import model.Room;
-import model.RoomType;
 import model.RoomStatus;
 import model.Hotel;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-public interface RoomService extends BaseService<Room, String> {
+public interface RoomService {
+    void create(Room room);
+    
     List<Room> findByHotel(Hotel hotel);
-    List<Room> findByType(Hotel hotel, RoomType type);
-    List<Room> findByStatus(Hotel hotel, RoomStatus status);
-    List<Room> findAvailableRooms(Hotel hotel, Date checkIn, Date checkOut);
-    List<Room> findAvailableRoomsByType(Hotel hotel, RoomType type, Date checkIn, Date checkOut);
-    boolean isRoomAvailable(Hotel hotel, String roomNumber, Date checkIn, Date checkOut);
-    void updateRoomStatus(Hotel hotel, String roomNumber, RoomStatus newStatus);
-    double getOccupancyRate(Hotel hotel, Date startDate, Date endDate);
-    List<Room> findRoomsNeedingCleaning(Hotel hotel);
+    
+    void updateRoomStatus(Hotel hotel, String roomNumber, RoomStatus status);
+    
     ArrayList<Room> getAvailableRooms(String checkInDate, String checkOutDate);
 }
